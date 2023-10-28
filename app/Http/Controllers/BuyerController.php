bb438dfe3e5e40a365463b68a2d3fcc37268b5b8
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-10-28 13:28:35 +0300

    add table Buyer Model

---
+++ app/Http/Controllers/BuyerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBuyerRequest;
+use App\Http\Requests\UpdateBuyerRequest;
+use App\Models\Buyer;
+
+class BuyerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBuyerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBuyerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBuyerRequest  $request
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBuyerRequest $request, Buyer $buyer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Buyer  $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buyer $buyer)
+    {
+        //
+    }
+}
