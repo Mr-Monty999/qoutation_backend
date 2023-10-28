178770519f11eafa94852c5c1c6aa9b6d95aa87e
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-10-28 14:46:02 +0300

    update api structer

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,7 +41,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::prefix('api')
                 ->middleware('api')
                 ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->group(base_path('routes/Api/v1.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
--- routes/Api/v1.php
+++ routes/Api/v1.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v1"], function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
