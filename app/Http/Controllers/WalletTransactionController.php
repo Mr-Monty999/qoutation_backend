0c161fa25eb5af561054be2f1227a8711e8b89e0
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-10-28 13:14:46 +0300

    Add WalletTransaction Model

---
+++ app/Http/Controllers/WalletTransactionController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreWalletTransactionRequest;
+use App\Http\Requests\UpdateWalletTransactionRequest;
+use App\Models\WalletTransaction;
+
+class WalletTransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreWalletTransactionRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreWalletTransactionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\WalletTransaction  $walletTransaction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WalletTransaction $walletTransaction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\WalletTransaction  $walletTransaction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WalletTransaction $walletTransaction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateWalletTransactionRequest  $request
+     * @param  \App\Models\WalletTransaction  $walletTransaction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateWalletTransactionRequest $request, WalletTransaction $walletTransaction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\WalletTransaction  $walletTransaction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WalletTransaction $walletTransaction)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_28_100855_create_wallet_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallet_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("wallet_id");
+            $table->string("type")->nullable();
+            $table->decimal("amount", 12)->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign("wallet_id")->references("id")->on("wallets")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallet_transactions');
+    }
+};
