17f6cd15f35980fde20a11265a28591d504b2233
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-10-29 12:39:05 +0300

    add ForceJson & Language middlewares & add some languages files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\ForceJsonResponse::class,
+            \App\Http\Middleware\LanguageMiddleware::class
         ],
     ];
 
