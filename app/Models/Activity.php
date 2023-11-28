4fbdfbfa3a3722d53c744c6401f167a21d1de94a
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-11-28 12:36:22 +0300

    Add Country Model

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Activity extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = ["id"];
+
     public function users()
     {
         return $this->belongsToMany(User::class, "user_activity", "activity_id");
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger("country_id")->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string("phone")->unique()->nullable();
@@ -24,6 +25,9 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+
+
+            $table->foreign("country_id")->references("id")->on("countries")->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
