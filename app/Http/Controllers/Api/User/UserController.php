dbc27770af5dae2d2061ca24f25172814f30381b
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-01-13 11:40:31 +0300

    update get-auth-user apit

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -13,10 +13,21 @@ class UserController extends Controller
     public function getAuthUser()
     {
         $user = Auth::user();
-        $user->load(["supplier", "buyer", "admin", "wallet"])->loadCount("notifications", "unreadNotifications");
+        $user->load(["supplier", "buyer", "admin", "wallet"])
+            ->loadCount(
+                "notifications",
+                "unreadNotifications"
+            );
+        $data = $user;
+        $data["unread_messages_count"] = MessageRecipient::where("receiver_id", $user->id)
+            ->whereNull("read_at")
+            ->count();
+        $data["sent_messages_count"] = Message::where("sender_id", $user->id)->count();
 
 
-        return response()->json($user);
+        return response()->json([
+            "data" => $data
+        ]);
     }
     public function info()
     {
