498a41d57781293b1c9980c4515ecbf8350418e3
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-01-14 13:35:25 +0300

    add perPage to Activities api

--- app/Http/Controllers/Api/Public/ActivityController.php
+++ app/Http/Controllers/Api/Public/ActivityController.php
@@ -6,12 +6,23 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreActivityRequest;
 use App\Http\Requests\UpdateActivityRequest;
 use App\Models\Activity;
+use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $activities = Activity::get();
+
+        $activities = null;
+        $perPage = 10;
+
+        if ($request->perPage)
+            $perPage = $request->perPage;
+
+        if ($request->has("paginated") && $request->paginated == "true")
+            $activities = Activity::paginate($perPage);
+        else
+            $activities = Activity::get();
 
         return response()->json($activities);
     }
