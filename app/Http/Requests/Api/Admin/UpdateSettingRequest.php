b2bef0a4a8ef59ca374def473a32d1af6f523421
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-02-26 15:04:16 +0300

    update UpdateSettingRequest

--- app/Http/Requests/Api/Admin/UpdateSettingRequest.php
+++ app/Http/Requests/Api/Admin/UpdateSettingRequest.php
@@ -22,6 +22,25 @@ class UpdateSettingRequest extends FormRequest
 
         $this->keys = implode(",", [
             "website_name",
+            "website_logo",
+            "home_page_text_1",
+            "home_page_text_2",
+            "home_page_text_3",
+            "home_page_text_4",
+            "home_page_text_5",
+            "home_page_text_6",
+            "home_page_button_1_text",
+            "home_page_button_1_url",
+            "home_page_feature_1_text",
+            "home_page_feature_2_text",
+            "home_page_feature_3_text",
+            "home_page_feature_3_text",
+            "home_page_feature_1_icon",
+            "home_page_feature_2_icon",
+            "home_page_feature_3_icon",
+            "home_page_feature_3_icon",
+            "footer_copyright_text",
+
         ]);
     }
 
