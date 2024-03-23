f719bf51f6bfcee1599a137c8b62e4a8e9a0aede
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-03-23 12:38:46 +0300

    add service & service reply models

---
+++ database/migrations/2024_03_23_113604_create_services_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("country_id")->nullable();
+            $table->unsignedBigInteger("city_id")->nullable();
+            $table->unsignedBigInteger("neighbourhood_id")->nullable();
+            $table->string("title")->nullable();
+            $table->string("description")->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            $table->foreign("user_id")->references("id")->on("users")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("country_id")->references("id")->on("countries")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("city_id")->references("id")->on("cities")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("neighbourhood_id")->references("id")->on("neighbourhoods")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
---
+++ database/migrations/2024_03_23_121052_create_service_replies_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_replies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("service_id");
+            $table->unsignedBigInteger("user_id");
+            $table->unsignedBigInteger("accepted_by")->nullable();
+            $table->decimal("price", 20, 2);
+            $table->string("title")->nullable();
+            $table->string("description")->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+
+
+
+
+
+            $table->foreign("service_id")->references("id")->on("services")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("user_id")->references("id")->on("users")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign("accepted_by")->references("id")->on("users")->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_replies');
+    }
+};
