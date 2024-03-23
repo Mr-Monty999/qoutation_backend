64b107ea82a5bb5ab14045fcbc56fda0f1c92ffe
Mr-Monty999 <montser.mohmed2022@gmail.com>
2024-03-23 14:41:36 +0300

    add only-buyer and only-supplier middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,9 @@ class Kernel extends HttpKernel
         'throttle' => \App\Http\Middleware\CustomThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         "only-admin" => \App\Http\Middleware\CheckIfIsAdmin::class,
-        "is-supplier-accepted" => \App\Http\Middleware\CheckIfSupplierIsAccepted::class
+        "is-supplier-accepted" => \App\Http\Middleware\CheckIfSupplierIsAccepted::class,
+        "only-buyer" => \App\Http\Middleware\OnlyBuyer::class,
+        "only-supplier" => \App\Http\Middleware\OnlySupplier::class
+
     ];
 }
